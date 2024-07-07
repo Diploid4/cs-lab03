547a599ddfc12f6d31a752e3307abee14117a235
Diploid4 <kostenko26slava@mail.ru>
2024-07-07 16:36:50 +0300

    SVG

--- Primer.cpp
+++ Primer.cpp
@@ -4,8 +4,8 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <string>
 using namespace std;
-
 
 vector<double> input_numbers(size_t count) {
     vector<double> result(count);
@@ -19,7 +19,6 @@ void find_minmax(vector<double> numbers, double& min, double& max) {
     min = *min_element(numbers.begin(), numbers.end());
     max = *max_element(numbers.begin(), numbers.end());
 }
-
 
 vector<size_t> make_histogram(vector<double> numbers, size_t bin_count) {
     vector<size_t> bins(bin_count);
@@ -65,7 +64,43 @@ void show_histogram_text(vector<size_t> bins) {
             cout << endl;
         }
 }
+void svg_text(double left, double baseline, string text) {
+    cout << "<text x='" << left << "' y='" << baseline << "'>" << text << "</text>" << '\n';
+}
+void svg_rect(double x, double y, double width, double height, string stroke = "red", string fill = "black") {
+    cout << "<rect x='" << x << "' y='" << y << "' width='" << width << "' height='" << height << "' stroke='" << stroke << "' fill='" << fill<<"' />" << '\n';
+}
 
+void svg_begin(double width, double height) {
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+void svg_end() {
+    cout << "</svg>\n";
+}
+
+void show_histogram_svg(const vector<size_t>& bins) {
+    const auto IMAGE_WIDTH = 400;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    const auto BLOCK_WIDTH = 10;
+    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+    double top = 0;
+    for (size_t bin : bins) {
+        const double bin_width = BLOCK_WIDTH * bin;
+        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
+        top += BIN_HEIGHT;
+    }
+    svg_end();
+}
 
 int main() {
     
@@ -77,7 +112,7 @@ int main() {
     cerr << "Enter bin count: ";
     cin >> bin_count;
     auto bins = make_histogram(numbers, bin_count);
-    show_histogram_text(bins);
+    show_histogram_svg(bins);
 }
 
 
